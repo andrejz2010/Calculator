614eed1d4d27a063d1d1eef6b6b0dc28827d0f44
Andrej Zacharov <zacharovandrej1@gmail.com>
2023-09-05 09:31:19 +0200

    Added option to user to add numbers.

--- src/Main.java
+++ src/Main.java
@@ -10,17 +10,22 @@ public class Main {
         int randomNumber2= random.nextInt(100);
         System.out.print("Please enter an operation \"+\", \"-\" or \"*\"   ");
         Scanner scanner = new Scanner(System.in);
+
         int result=0;
         String input = scanner.next();
+        System.out.print("Please enter first number:   ");
+        double number1 = scanner.nextDouble();
+        System.out.print("Please enter second number:   ");
+        double number2 = scanner.nextDouble();
         switch (input){
             case "+":
-                System.out.println(randomNumber1+randomNumber2);
+                System.out.println(number1+number2);
                 break;
             case "-":
-                System.out.println(randomNumber1-randomNumber2);
+                System.out.println(number1-number2);
                 break;
             case "*":
-                System.out.println(randomNumber1*randomNumber2);
+                System.out.println(number1*number2);
                 break;
             default:
                 System.out.println("Error. The operation is unclear.");

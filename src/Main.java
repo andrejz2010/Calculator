353062c4780609c7f4d2871ecb7354517b9055a3
Andrej Zacharov <zacharovandrej1@gmail.com>
2023-09-05 09:23:33 +0200

    Added a choice of operations on random numebrs

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,31 @@
+import java.sql.SQLOutput;
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Random random= new Random();
+        int randomNumber1= random.nextInt(100);
+        int randomNumber2= random.nextInt(100);
+        System.out.print("Please enter an operation \"+\", \"-\" or \"*\"   ");
+        Scanner scanner = new Scanner(System.in);
+        int result=0;
+        String input = scanner.next();
+        switch (input){
+            case "+":
+                System.out.println(randomNumber1+randomNumber2);
+                break;
+            case "-":
+                System.out.println(randomNumber1-randomNumber2);
+                break;
+            case "*":
+                System.out.println(randomNumber1*randomNumber2);
+                break;
+            default:
+                System.out.println("Error. The operation is unclear.");
+        }
+
+
     }
 }

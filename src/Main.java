24f0b609565ed490b51583212f3c7850a588be31
Andrej Zacharov <zacharovandrej1@gmail.com>
2023-09-06 17:46:16 +0200

    Added calculation interface. Organised different calc methods in separate classes.

--- src/Main.java
+++ src/Main.java
@@ -4,37 +4,61 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-
+        double result=0;
+        Scanner scanner = new Scanner(System.in);
 
 
         System.out.print("Please enter an operation \"+\", \"-\", \"/\" or \"*\"   ");
-        Scanner scanner = new Scanner(System.in);
+        String operator = scanner.next();
 
-        double result=0;
-        String input = scanner.next();
-        System.out.print("Please enter first number:   ");
-        double number1 = scanner.nextDouble();
-        System.out.print("Please enter second number:   ");
-        double number2 = scanner.nextDouble();
-        switch (input){
+        System.out.print("How many numbers?   ");
+        int numberOfNumbers = scanner.nextInt();
+        if (numberOfNumbers<=1){
+            System.out.println("You need to enter at least 2 numbers");
+            System.exit(0);
+
+        }
+
+        double[] numbersInArray = new double[numberOfNumbers];
+
+        for (int i = 0; i < numberOfNumbers; i++) {
+            System.out.printf("Please enter number %d:   %n", i+1);
+            double number = scanner.nextDouble();
+            numbersInArray[i]= number;
+        }
+
+        double NewResult=numbersInArray[0];
+        switch (operator){
             case "/":
                 Divide divide =new Divide();
-                result=divide.calculate(number1,number2);
+                for (int i = 0; i < numbersInArray.length-1; i++) {
+                    NewResult=divide.calculate(NewResult,numbersInArray[i+1]);
+                    result = NewResult;
+                }
                 System.out.println(result);
                 break;
             case "+":
-                Add add = new Add();
-                result=add.calculate(number1, number2);
+                Add add =new Add();
+                for (int i = 0; i < numbersInArray.length-1; i++) {
+                    NewResult=add.calculate(NewResult,numbersInArray[i+1]);
+                    result = NewResult;
+                }
                 System.out.println(result);
                 break;
             case "-":
-                Subtract subtract = new Subtract();
-                result=subtract.calculate(number1, number2);
+                Subtract subtract =new Subtract();
+                for (int i = 0; i < numbersInArray.length-1; i++) {
+                    NewResult=subtract.calculate(NewResult,numbersInArray[i+1]);
+                    result = NewResult;
+                }
                 System.out.println(result);
                 break;
             case "*":
-                Multiply multiply = new Multiply();
-                result=multiply.calculate(number1,number2);
+                Multiply multiply =new Multiply();
+                for (int i = 0; i < numbersInArray.length-1; i++) {
+                    NewResult=multiply.calculate(NewResult,numbersInArray[i+1]);
+                    result = NewResult;
+                }
                 System.out.println(result);
                 break;
             default:
